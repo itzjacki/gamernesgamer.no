a58899b1e4c3a43d0485e4010f75be0281ec399e
Jakob Kielland <jakob-ek@hotmail.com>
2023-09-21 22:33:40 +0200

    Add basics and participants section

---
+++ src/gamers.ts
@@ -0,0 +1,95 @@
+export interface Gamer {
+  name: string;
+  nickname: string;
+  imagePath: string;
+  overallRating: number;
+  stats: GamerStats;
+}
+
+export interface GamerStats {
+  focus: number;
+  strategy: number;
+  mechanics: number;
+  mental: number;
+  versatility: number;
+  consistency: number;
+  tactics: number;
+}
+
+export const gamers: Gamer[] = [
+  {
+    name: "Jakob Endrestad Kielland",
+    nickname: "The Man",
+    imagePath: "images/image.png",
+    stats: {
+      focus: 74,
+      strategy: 99,
+      mechanics: 77,
+      mental: 83,
+      versatility: 94,
+      consistency: 86,
+      tactics: 95,
+    },
+    overallRating: 87,
+  },
+  {
+    name: "Jørgen Endrestad Kielland",
+    nickname: "The Dude",
+    imagePath: "images/image.png",
+    stats: {
+      focus: 96,
+      strategy: 87,
+      mechanics: 98,
+      mental: 83,
+      versatility: 76,
+      consistency: 81,
+      tactics: 91,
+    },
+    overallRating: 87,
+  },
+  {
+    name: "Tobias Knapp Eliassen",
+    nickname: "The Male",
+    imagePath: "images/image.png",
+    stats: {
+      focus: 99,
+      strategy: 89,
+      mechanics: 86,
+      mental: 88,
+      versatility: 86,
+      consistency: 91,
+      tactics: 95,
+    },
+    overallRating: 91,
+  },
+  {
+    name: "William Grimholt",
+    nickname: "The Guy",
+    imagePath: "images/image.png",
+    stats: {
+      focus: 93,
+      strategy: 73,
+      mechanics: 95,
+      mental: 96,
+      versatility: 76,
+      consistency: 79,
+      tactics: 77,
+    },
+    overallRating: 84,
+  },
+  {
+    name: "Kristin Helene Lie Størkson",
+    nickname: "The Gal",
+    imagePath: "images/image.png",
+    stats: {
+      focus: 80,
+      strategy: 93,
+      mechanics: 79,
+      mental: 93,
+      versatility: 94,
+      consistency: 95,
+      tactics: 80,
+    },
+    overallRating: 88,
+  },
+];
