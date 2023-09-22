97616dd2571d745b37a692cffcbd01080371cc54
Jakob Kielland <jakob-ek@hotmail.com>
2023-09-23 01:31:34 +0200

    update gamers

--- src/gamers.ts
+++ src/gamers.ts
@@ -1,7 +1,7 @@
 export interface Gamer {
   name: string;
   nickname: string;
-  imagePath: string;
+  imagePath: string; // should be 1000px tall
   overallRating: number;
   stats: GamerStats;
 }
@@ -18,39 +18,9 @@ export interface GamerStats {
 
 export const gamers: Gamer[] = [
   {
-    name: 'Jakob Endrestad Kielland',
-    nickname: '"The Professor"',
-    imagePath: 'images/image.png',
-    stats: {
-      focus: 74,
-      strategy: 99,
-      mechanics: 77,
-      mental: 83,
-      versatility: 94,
-      consistency: 86,
-      tactics: 95,
-    },
-    overallRating: 87,
-  },
-  {
-    name: 'Jørgen Endrestad Kielland',
-    nickname: '"All in"',
-    imagePath: 'images/image.png',
-    stats: {
-      focus: 96,
-      strategy: 87,
-      mechanics: 98,
-      mental: 83,
-      versatility: 76,
-      consistency: 81,
-      tactics: 91,
-    },
-    overallRating: 87,
-  },
-  {
     name: 'Tobias Knapp Eliassen',
     nickname: '"The Maestro"',
-    imagePath: 'images/image.png',
+    imagePath: 'images/gamers/tobias.png',
     stats: {
       focus: 99,
       strategy: 89,
@@ -63,9 +33,40 @@ export const gamers: Gamer[] = [
     overallRating: 91,
   },
   {
+    name: 'Jørgen Endrestad Kielland',
+    nickname: '"All in"',
+    imagePath: 'images/gamers/jorgen.png',
+    stats: {
+      focus: 96,
+      strategy: 87,
+      mechanics: 98,
+      mental: 83,
+      versatility: 76,
+      consistency: 81,
+      tactics: 91,
+    },
+    overallRating: 87,
+  },
+
+  {
+    name: 'Jakob Endrestad Kielland',
+    nickname: '"The Professor"',
+    imagePath: 'images/gamers/jakob.png',
+    stats: {
+      focus: 74,
+      strategy: 99,
+      mechanics: 77,
+      mental: 83,
+      versatility: 94,
+      consistency: 86,
+      tactics: 95,
+    },
+    overallRating: 87,
+  },
+  {
     name: 'William Grimholt',
     nickname: '"Iron Will"',
-    imagePath: 'images/image.png',
+    imagePath: 'images/gamers/william.png',
     stats: {
       focus: 93,
       strategy: 73,
@@ -77,19 +78,19 @@ export const gamers: Gamer[] = [
     },
     overallRating: 84,
   },
-  {
-    name: 'Kristin Helene Lie Størkson',
-    nickname: '"Sleeping Tiger"',
-    imagePath: 'images/image.png',
-    stats: {
-      focus: 80,
-      strategy: 93,
-      mechanics: 79,
-      mental: 93,
-      versatility: 94,
-      consistency: 95,
-      tactics: 80,
-    },
-    overallRating: 88,
-  },
+  // {
+  //   name: 'Kristin Helene Lie Størkson',
+  //   nickname: '"Sleeping Tiger"',
+  //   imagePath: 'images/image.png',
+  //   stats: {
+  //     focus: 80,
+  //     strategy: 93,
+  //     mechanics: 79,
+  //     mental: 93,
+  //     versatility: 94,
+  //     consistency: 95,
+  //     tactics: 80,
+  //   },
+  //   overallRating: 88,
+  // },
 ];
