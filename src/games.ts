0b1f46c0f372033ee4a887ebe21c1212464f17cd
Jakob Kielland <jakob-ek@hotmail.com>
2023-10-03 22:59:23 +0200

    Update game info

--- src/games.ts
+++ src/games.ts
@@ -18,6 +18,31 @@ export const games: Game[] = [
     duration: '30 min',
     shortDescription:
       'Head-to-Head-turnering med decks laget av kun basickortene som alle har.',
+    longDescription: `
+    <p>
+      Hver utøver har mulighet til å lage opptil 2 standard-deck på helt nye 
+      hearthstonebrukere i forberedelsene til GG. Eneste begrensning i prep-time 
+      er at man ikke kan bruke deck templates fra nettet eller bruke ekte penger 
+      på den brukeren sin. Deckene skal ikke reveales i forkant av turneringen. 
+    </p>
+    <br>
+    <p>
+      Underveis i gruppespillet har man heller ikke lov til å spectate kampen som 
+      foregår parallelt for å få info om andres deck. Gruppespillet består av 1 
+      "round robin". Deretter blir det bronsefinale og finale for å avgjøre 
+      endelig plassering.
+    </p>
+    <br>
+    <p>
+      Ved "2 way tie" i gruppespillet blir det avgjørt av en 
+      "head to head" med redusert tid per trekk. Ved "3 way tie" blir det avgjort 
+      av hvor mange turns man overlevde i tapte match(er). 
+    </p>
+    <br>
+    <p>
+      Death knight er bannet.
+    </p>
+    `,
   },
   {
     title: 'Curve Fever',
@@ -96,6 +121,65 @@ export const games: Game[] = [
     duration: '60 min',
     shortDescription:
       'Så mange races vi rekker på 60 minutter med poengscoring basert på plassering.',
+    longDescription: `
+      <p>
+        Vi spiller opp til 10 races, 5 skikkelige races med Boomer, 5 tullete 
+        races med 5 litt mer spesielle biler. Antall races totalt avhenger av 
+        hvor mye tid vi har. Vi kjører skikkelige og tullete races annenhver 
+        gang. Man kan tune alle bilene akkurat hvordan man vil. Alle skikkelige
+        races er 5 laps, mens alle tulleraces er 3 laps. Vi spiller uten bots. 
+        De skikkelige racesa kjøres på de følgende banene:
+      </p>
+      <br>
+      <ul>
+        <li>- Espedalen</li>
+        <li>- Sandstone</li>
+        <li>- Hillstreet</li>
+        <li>- Finncross</li>
+        <li>- Boulder bank</li>
+      </ul>
+      <br>
+      <p>
+        De tullete racesa randomizes ved at en av de følgende banene:
+      </p>
+      <br>
+      <ul>
+        <li>- Hellride</li>
+        <li>- Maasten Motorcenter</li>
+        <li>- Wrecknado</li>
+        <li>- Rockfield Roughspot</li>
+        <li>- Dirt Devil Stadium</li>
+      </ul>
+      <br>
+      <p>
+        Matches med en av de følgende bilene:
+      </p>
+      <br>
+      <ul>
+        <li>- Combine harvester</li>
+        <li>- Lawnmower</li>
+        <li>- Supervan</li>
+        <li>- Sofa car</li>
+        <li>- Motorhome</li>
+      </ul>
+      <br>
+      <p>
+        Poengene fordeles slik:
+      </p>
+      <br>
+      <ul>
+        <li>- 1. plass: 4 poeng</li>
+        <li>- 2. plass: 3 poeng</li>
+        <li>- 3. plass: 2 poeng</li>
+        <li>- 4. plass: 1 poeng</li>
+      </ul>
+      <br>
+      <p>
+        Man får også 1 ekstrapoeng for raskeste rundetid i hvert race. Vinneren
+        er den som har flest poeng totalt på slutten av siste race.
+    `,
+    videoEmbedUrl:
+      'https://www.youtube.com/embed/V3l56tS_yms?si=g3QNROSZgE9BZr9N',
   },
   {
     title: 'Total War: Empire',
